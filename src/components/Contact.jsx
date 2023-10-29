b3113dcdf505c749bbd5a0fa94e7ed01769551ed
unknown <moccia.ant@gmail.com>
2023-10-29 10:42:20 +0100

    terminato per desktop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,16 +6,21 @@ function Contact() {
       <div className='section-contacts left'>
         <form className='contact-form'>
           <label htmlFor='email'>Email</label>
-          <input id='email' />
+          <input type='text' id='email' />
           <label htmlFor='nome'>Nome e cognome</label>
-          <input id='nome' />
+          <input type='text' id='nome' />
           <label htmlFor='testo'>Testo</label>
-          <input id='testo' />
+          <textarea id='testo' />
           <button>Invia</button>
         </form>
       </div>
       <div className='section-contacts right'>
-        contact info
+        <h4>Email</h4>
+        <p>info@oxfordbureau.it</p>
+        <h4>Phone Number</h4>
+        <p>+39 3936545133</p>
+        <h4>P.Iva</h4>
+        <p>12345678945</p>
       </div>
     </div>
   )
